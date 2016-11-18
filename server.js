5583df7668df0f0f592652d7e8bbda3e84032a69
yash khare <khareyash19@gmail.com>
2016-11-19 03:28:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ var Pool = require('pg').Pool;
 //DATABASE CONFIGURATION
 var config = {
   host: 'db.imad.hasura-app.io',
-  user: 'khare19yash',
+  user: 'itsinayats',
   password: process.env.DB_PASSWORD,
-  database: 'khare19yash',
+  database: 'itsinayats',
   port:'5432'
 };
 
@@ -26,100 +26,10 @@ app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new Pool(config);
-
-//shubham server.js file
-
-function hash (input, salt) {
-    // How do we create a hash?
-    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
-}
-
-
-app.get('/hash/:input', function(req, res) {
-   var hashedString = hash(req.params.input, 'this-is-some-random-string');
-   res.send(hashedString);
-});
-
-app.post('/create-user', function (req, res) {
-   // username, password
-   // {"username": "tanmai", "password": "password"}
-   // JSON
-   var username = req.body.username;
-   var password = req.body.password;
-   var salt = crypto.randomBytes(128).toString('hex');
-   var dbString = hash(password, salt);
-   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          res.send('User successfully created: ' + username);
-      }
-   }); 
-});
-
-app.post('/', function (req, res) {
-   var username = req.body.username;
-   var password = req.body.password;
-   
-   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          if (result.rows.length === 0) {
-              res.status(403).send('username/password is invalid');
-          } else {
-              // Match the password
-              var dbString = result.rows[0].password;
-              var salt = dbString.split('$')[2];
-              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
-              if (hashedPassword === dbString) {
-                
-                // Set the session
-                req.session.auth = {userId: result.rows[0].id};
-                // set cookie with a session id
-                // internally, on the server side, it maps the session id to an object
-                // { auth: {userId }}
-                
-                res.send('credentials correct!');
-                
-              } else {
-                res.status(403).send('username/password is invalid');
-              }
-          }
-      }
-   });
-});
-
-app.get('/check-login', function (req, res) {
-   if (req.session && req.session.auth && req.session.auth.userId) {
-       // Load the user object
-       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
-           if (err) {
-              res.status(500).send(err.toString());
-           } else {
-              res.send(result.rows[0].username);    
-           }
-       });
-   } else {
-       res.status(400).send('You are not logged in');
-   }
-});
-
-app.get('/logout', function (req, res) {
-   delete req.session.auth;
-   res.send('<http><head><meta http-equiv="Refresh" content="1; /"<h1>Logged Out</h1>' );
-});
-
-
-
-
-
 //CONNECTION CREATION
 var pool=new Pool(config);
-app.get('/articles', function (req, res) {
-    pool.query('select * from article',function(err,result){
+app.get('/test-db', function (req, res) {
+    pool.query('select * from articles',function(err,result){
         if(err)
         {
           res.status(500).send(err.toString()) ;
@@ -132,7 +42,10 @@ app.get('/articles', function (req, res) {
  
 });
 
-
+//load blog
+app.get('/blog', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'blog.html'));
+});
 
 //BOOTSTRAP FILES INCLUDES
 app.get('/ui/css/bootstrap.min.css', function (req, res) {
@@ -150,6 +63,10 @@ app.get('/ui/js/bootstrap.min.js', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+//article.js
+app.get('/ui/article.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article.js'));
+});
 //favicon
 app.get('/fevicon.ico', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
@@ -160,20 +77,10 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 
-
-//blog
-app.get('/blog', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui/blog', 'blog.html'));
-});
-
-
-
-
-
-
-
-
 //images
+app.get('/ui/images/spinner.gif', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/images', 'spinner.gif'));
+});
 
 app.get('/ui/images/inayat.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui/images', 'inayat.jpg'));
@@ -223,44 +130,23 @@ app.get('/ui/images/twitter.PNG', function (req, res) {
 });
 
 app.get('/ui/images/a.gif',function(req,res){
-res.sendFile(path.join(__dirname, 'ui/images' , 'a.gif'))
+res.sendFile(path.join(__dirname, 'ui/images' , 'a.gif'));
 });
 app.get('/ui/images/fb.jpg',function(req,res){
-res.sendFile(path.join(__dirname, 'ui/images' , 'fb.jpg'))
+res.sendFile(path.join(__dirname, 'ui/images' , 'fb.jpg'));
 });
 app.get('/ui/images/gl.jpg',function(req,res){
-res.sendFile(path.join(__dirname, 'ui/images' , 'gl.jpg'))
+res.sendFile(path.join(__dirname, 'ui/images' , 'gl.jpg'));
 });
 
 
-//counter
-var counter=0;
-app.get('/counter',function(req, res){
-counter = counter + 1;
-res.send(counter.toString());
+/*
 
-});
-
- //CODE FOR GETTING names
-var names=[];
-//app.get('/submit_name/:name',function(req,res){
-app.get('/submit_name',function(req,res){
-//var name=req.params.name;   //way 1
-var name=req.query.name;
-names.push(name);
-res.send(JSON.stringify(names)); 
-});
-
-
-
-
-
-//articles
-
+//app.get('/blog', function (req, res) {
 app.get('/articles/:articleName', function (req, res) {
 // var articleName=req.params.articleName;
 //pool.query("Select * from articles,users where title='" + req.params.articleName + "'", function(err,result){
-pool.query("select article_tags.tag,articles.title,articles.content,articles.category,articles.heading, articles,date,users.name from articles,article_tags,users where article_tags.article_id=articles.id AND articles.title='"+ req.params.articleName +"' AND articles.author_id=users.id;",function(err,result){
+pool.query("select article_tags.tag,articles.title,articles.content,articles.category,articles.heading, articles,date,users.name from articles,article_tags,users where article_tags.article_id=articles.id AND articles.title='"+ req.params.articleName +"' AND articles.author_id=users.id  order by time DESC",function(err,result){
     
 
     
@@ -275,242 +161,60 @@ pool.query("select article_tags.tag,articles.title,articles.content,articles.cat
              }
                  else{
                 
-                  var  articleData=result.rows[0];
-                  res.send(createTemplate(articleData));
+                 res.send(JSON.stringify(result.rows));
                      
                  }
              }
+    
+});
+});
+ 
+
+*/
+
+
+//getting categories
+app.get('/get-categories', function (req, res) {
+   pool.query('SELECT * FROM category', function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result.rows));
+      }
+   });
 });
 
- });
 
 
-//TEMPLATE CODE
 
-function createTemplate(data){
-var title=data.title;
-var date=data.date;
-var heading=data.heading;
-var content=data.content;
-var category=data.category;
-var author=data.name;
-var tag=data.tag;
-var template= `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>${title}</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="ui/style.css">
+//fetching recent article on blog home page
+app.get('/get-blog-data',function(req,res){
+    pool.query("select articles.title,articles.content,articles.category,articles.heading, articles.time,users.name from articles,article_tags,users where articles.author_id=users.id order by time DESC",function(err,result){
+         if(err){
+          res.status(500).send(err.toString()) ;
+          }
+     else{
+            if(result.rows.length===0)
+             {
+                res.status(400).send('ARTICLE NOT FOUND');
+             }
+                 else{
+                   res.send(JSON.stringify(result.rows));
+                     
+                 }
+             }
+        
+    });
+    
+});
+
+//get-tags
+
  
-  		<style>
 
-  		#leftCol{
-  		    
-  		    z-index:1;
-  
-  		}
-  .affix {
-      top: 0;
-      width: 100%;
-  }
-  .s {
-     width:100px; 
-  }
-
- .affix + .container-fluid {
-      padding-top: 70px;
-  }
-  .bold{
-      font-weight:bolder;
-  }
-</style>
-</head>
-<body>
- <!--NAVBAR STARTS  -->
-    <nav class="navbar navbar-inverse" role="navigation" data-spy="affix" style="z-index:2">
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-       <a href="#" class="active navbar-brand bold">Inayat's Blog</a>
-       
-    </div>
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-            <li class=" bold"><a href="#">Home</a></li>
-            <li><a href="#" class="bold" data-toggle="modal" data-target="#loginModal">Login</a></li>
-           <li><a href="#" class="bold" data-toggle="modal" data-target="#registerModal">Register</a></li>
-           </ul>
-           <div class="col-sm-3 col-md-3 pull-right">
-            <form class="navbar-form" role="search">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search" name="q">
-                    <div class="input-group-btn">
-                        <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-                    </div>
-                </div>
-            </form>
-        </div>        
-    </div>
-</nav>
-  <!--NAVBAR ENDS  -->
-  
-   <div class="container-fluid">
-	<div class="row">
-	  <!--left-->
-      <div class="col-md-3" id="leftCol">
-      <div class="nav nav-stacked affix s" style="position:fixed">
-   
-      <button class=" btn btn-lg btn-info" data-toggle="collapse" data-target="#cat" style="width:320px;text-align:center;margin-top:50px">Categories</button>
-      <div id="cat" class="collapse"z-index:1">
-  <ul class="list-group" style="width:320px;">
-  <li class="list-group-item">New <span class="badge">12</span></li>
-  <li class="list-group-item">Deleted <span class="badge">5</span></li> 
-  <li class="list-group-item">Warnings <span class="badge">3</span></li> 
-       </ul>
-         
-        </div>
-        </div>
-      </div><!--/left-->
-    
-      <div class="col-sm-9">
-    
-      <h4><small>RECENT POSTS</small></h4>
-      <hr>
-      <h1>${heading.toUpperCase()}</h1>
-       <h4><span class="label label-info">${category}</span></h4>
-      <h5><span class="glyphicon glyphicon-time"></span> Post by <b>${author}</b>, ${date.toDateString()}.</h5>
-      <h5><span class="label label-success">${tag[1]}</span></h5><br>
-      <p>${content}</p>
-      <hr>
-
-      <h4>Leave a Comment:</h4>
-      
-      <form role="form">
-        <div class="form-group">
-          <textarea class="form-control" rows="3" required></textarea>
-        </div>
-        <button type="submit" class="btn btn-success">Submit</button>
-      </form>
-      <br><br>
-      
-      <p><span class="badge">2</span> Comments:</p><br>
-      
-      <div class="row">
-        <div class="col-sm-2 text-center">
-          <img src="bandmember.jpg" class="img-circle" height="65" width="65" alt="Avatar">
-        </div>
-        <div class="col-sm-10">
-          <h4>Anja <small>Sep 29, 2015, 9:12 PM</small></h4>
-          <p>Keep up the GREAT work! I am cheering for you!! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <br>
-        </div>
-        <div class="col-sm-2 text-center">
-          <img src="bird.jpg" class="img-circle" height="65" width="65" alt="Avatar">
-        </div>
-        <div class="col-sm-10">
-          <h4>John Row <small>Sep 25, 2015, 8:25 PM</small></h4>
-          <p>I am so happy for you man! Finally. I am looking forward to read about your trendy life. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <br>
-          <p><span class="badge">1</span> Comment:</p><br>
-          <div class="row">
-            <div class="col-sm-2 text-center">
-              <img src="bird.jpg" class="img-circle" height="65" width="65" alt="Avatar">
-            </div>
-            <div class="col-xs-10">
-              <h4>Nested Bro <small>Sep 25, 2015, 8:28 PM</small></h4>
-              <p>Me too! WOW!</p>
-              <br>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-  <!-- LoginModal -->
-  <div class="modal fade" id="loginModal" role="dialog">
-    <div class="modal-dialog">
-    
-
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
-        </div>
-        <div class="modal-body">
-          <p>Some text in the modal.</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-      
-    </div>
-  </div>
-  
-  
-  
-  
-    <!-- Register Modal -->
-  <div class="modal fade" id="registerModal" role="dialog">
-    <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
-        </div>
-        <div class="modal-body">
-          <p>Some text in the modal.</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-      
-    </div>
-  </div>
-  
-</div>
-
-
-
-
-
-
-
-
-
-<footer class="container-fluid">
-  <p>Footer Text</p>
-</footer>
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="/ui/main.js"></script>
-</body>
-</html>
-
-`;
-
-return template;
-}
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
  console.log(`IMAD course app listening on port ${port}!`);
 });
-
-
-
