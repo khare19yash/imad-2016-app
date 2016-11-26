c8dcd8acf45aae3eca442ff35debb09f0ac97ecb
yash khare <yash khare>
2016-11-26 22:04:12 +0530

    new commit

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var config = {
-    user: 'abhilashkasap',
-    database: 'abhilashkasap',
+    user: 'khare19yash',
+    database: 'khare19yash',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
