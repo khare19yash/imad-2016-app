a84b17ff6dc15095ed632d034466086fb4bfdeb4
yash khare <khareyash19@gmail.com>
2016-11-26 14:52:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -236,27 +236,7 @@ function loadLoginForm () {
         
     };
     
-    /*var register = document.getElementById('registerbtn');
-    register.onclick = alert('Could not register the user');*/
-   /* function myFunction () {
-        alert("hi");
-        // Create a request object
-        /*var request = new XMLHttpRequest();
-        
-        // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-              // Take some action
-              if (request.status === 200) {
-                  alert('User created successfully');
-                  register.value = 'Registered!';
-              } else {
-                  alert('Could not register the user');
-                  register.value = 'Register';
-              }
-          }*/
-     //   };*/
-        
+
         // Make the request
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
