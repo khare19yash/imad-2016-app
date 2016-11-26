5ac69ad92f49aedb47b46f74709a297f60b09f51
yash khare <khareyash19@gmail.com>
2016-11-26 15:36:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 var pool=new Pool(config);
 app.get('/test-db', function (req, res) {
     pool.query('SELECT * FROM test-db',function(err,result){
-        if(err)
+        if (err)
         {
             res.ststus(500).send(err.toString());
         }
