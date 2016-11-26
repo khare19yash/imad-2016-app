7b7061c293558f1672ef124b44c3a676d88d2c40
yash khare <yash khare>
2016-11-26 22:01:18 +0530

    new commit

--- server.js
+++ server.js
@@ -2,35 +2,78 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-var bodyParser = require('body-parser');
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 var session = require('express-session');
 
-
-//remember to change the password field before deploying
-var config ={
-  user:'khare19yash',
-  database:'khare19yash',
-  host:'db.imad.hasura-app.io',
-  port: '5432',
-  password: process.env.DB_PASSWORD
-
+var config = {
+    user: 'abhilashkasap',
+    database: 'abhilashkasap',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
 };
 
 var app = express();
-//app.use(morgan('combined'));
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
     secret: 'someRandomSecretValue',
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
+var pool = new Pool(config);
 
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `<!DOCTYPE html>
+                         <head>
+          <title>
+        ${title} </title>
+    <link rel="stylesheet" type="text/css" href="/ui/blogstyle.css">
+    <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Architects+Daughter" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Slab" rel="stylesheet">
+</head>
 
-/*app.use(express.static(__dirname + '/ui'));
+<body>
+    <div id="header">
+        <span id="home"><a class="anchor" href="/">Home</a></span>
+        <span id="myblog">Vikshipt's Blog</span>
+       <span  style="margin-left:45%; font-size:20px; font-family: 'Architects Daughter', cursive;" id="userinfo">
+            <strong> Hi Guest !</strong>
+        </span>
+    </div>
+    <div id="articlelist">
+    </div>
+    <div id="dycontent">
+    <div id="maintext">
+        ${content}
+   
+        </div>
+        <div id="comment_form">
+          
+    </div>
+    <div id="comments">
+    <br> <span style="font-size:26px"><strong>Comments</strong></span><br>
+      Loading Comments....
+    </div>
+    </div>
+    
 
-app.set('views', path.join(__dirname, '/ui'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html'); */
+<script type="text/javascript" src="/ui/article.js"></script>
+</body>
+
+</html> `;
+    return htmlTemplate;
+}
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
 function hash (input, salt) {
     // How do we create a hash?
@@ -38,198 +81,28 @@ function hash (input, salt) {
     return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
 }
 
-function createTemplate(data){
 
-  var heading = data.heading;
-  var date = data.date;
-  var content= data.content;
-  var author = data.author;
-
-
-  var htmlTemplate= `
-    <!doctype html>
-<html>
-<head>
-  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <link href="/ui/articlestyle.css" rel="stylesheet" />
-
-
-  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-  <link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.deep_purple-blue.min.css" />
-
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-  <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
-
-
-
-  <script src="https://code.getmdl.io/1.2.1/material.min.js"></script>
-  <script type="text/javascript" src="/ui/article.js"></script>
-
-
-
-
-</head>
-<body>
-  <!-- Always shows a header, even in smaller screens. -->
-  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-   <header class="mdl-layout__header">
-    <div class="mdl-layout__header-row">
-      <!-- Title -->
-
-      <!-- Add spacer, to align navigation to the right -->
-      <div class="mdl-layout-spacer"></div>
-      <!-- Navigation. We hide it in small screens. -->
-      <nav class="mdl-navigation mdl-typography--body-1-force-preferred-font">
-          <a class="mdl-navigation__link" href="/"><h6>Home</h6></a>
-          <a class="mdl-navigation__link" href="/login"><h6>Blog</h6></a>
-          <a class="mdl-navigation__link" href="/publish-article"><h6>Publish Article</h6></a>
-          <a class="mdl-navigation__link" href="/readroom"><h6>Read Room</h6></a>
-      </nav>
-    </div>
-  </header>
-  <main class="mdl-layout__content">
-    <div class="mdl-grid">
-      <div class="mdl-cell mdl-cell--1-col"></div>
-      <div class="mdl-cell mdl-cell--5-col">
-        <div class="container">
-          <h2> ${heading} </h2> <br>
-          <p> Written by </p> <a href=""> ${author} </a>  <p> ${date.toDateString()}</p>
-          
-
-          <p>${content}</p>
-
-          <h4>Comments</h4>
-            <div id="comment_form">
-            </div>
-            <div id="comments">
-              <!--<center>Loading comments...</center> -->
-            </div>
-
-         <hr>
-       </div>
-
-   </div>
-   <div class="mdl-cell mdl-cell--5-col">
-     
-  </div>
-  <div class="mdl-cell mdl-cell--1-col"></div>
-</div>
-
-<footer class="mdl-mini-footer">
-  <div class="mdl-mini-footer__left-section">
-    <div class="mdl-logo">
-      <h6>Application developed as part of IMAD </h6>
-    </div>
-  </div>
-
-  <div class="mdl-mini-footer__right-section">
-
-    <ul class="mdl-mini-footer__link-list">
-      <li> <h5><a href="https://www.hackerearth.com/@sumant2" target="_blank"><i class="fa fa-linkedin fa-lg" aria-hidden="true"></i></a></h5></li>
-
-      <li> <h5><a href="https://www.hackerearth.com/@sumant2" target="_blank"><i class="fa fa-header fa-lg"></i></a></h5></li>
-      <li><h5><a href="https://github.com/Subway19" target="_blank"><i class="fa fa-github fa-lg"></i></a></h5></li>
-      <li>  <h5><a href="https://www.facebook.com/sumantbagade19" target="_blank"><i class="fa fa-facebook fa-lg"></i></a></h5>
-      </li>
-    </ul>
-
-  </div>
-</footer>
-</main>
-</div>
-</body>
-</html>
-
-  `
-
-  return htmlTemplate;
-}
-
-
-
-
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','about.html'));
-
-
-  
+app.get('/hash/:input', function(req, res) {
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
 });
 
-var pool = new Pool(config);
-
-app.get('/articles/:articleName', function (req, res) {
-
-
-  var articleName = req.params.articleName;
-
-  pool.query("SELECT * FROM article WHERE title = $1", [articleName] , function(err,result){
-    if(err){
-      res.status(500).send(err.toString());
-    }
-    else{
-      if(result.rows.length==0){
-        res.status(404).send('Article not found');
-      }
-      else{
-        var articleData = result.rows[0];
-        res.send(createTemplate(articleData));
-      }
-    }
-
-  });
-
-  
-});
-
-
-app.get('/get-articles', function (req, res) {
-   // make a select request
-   // return a response with the results
-   pool.query('SELECT * FROM article ORDER BY date DESC', function (err, result) {
+app.post('/create-user', function (req, res) {
+   // username, password
+   // {"username": "tanmai", "password": "password"}
+   // JSON
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
-          res.send(JSON.stringify(result.rows));
+          res.send('User successfully created: ' + username);
       }
    });
 });
-
-app.get('/get-comments/:articleName', function (req, res) {
-   // make a select request
-   // return a response with the results
-   pool.query('SELECT comment.*, "user".username FROM article, comment, "user" WHERE article.title = $1 AND article.id = comment.article_id AND comment.user_id = "user".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          res.send(JSON.stringify(result.rows));
-      }
-   });
-});
-
-
-
-
-
-
-
-app.get('/check-login', function (req, res) {
-   if (req.session && req.session.auth && req.session.auth.userId) {
-       // Load the user object
-       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
-           if (err) {
-              res.status(500).send(err.toString());
-           } else {
-              res.send(result.rows[0].username);    
-           }
-       });
-   } else {
-       res.status(400).send('You are not logged in');
-   }
-});
-
 
 app.post('/login', function (req, res) {
    var username = req.body.username;
@@ -264,56 +137,51 @@ app.post('/login', function (req, res) {
    });
 });
 
-app.post('/create-user', function (req, res) {
-   // username, password
-   // {"username": "tanmai", "password": "password"}
-   // JSON
-   var username = req.body.username;
-   var password = req.body.password;
-   var salt = crypto.randomBytes(128).toString('hex');
-   var dbString = hash(password, salt);
-   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
+});
+
+app.get('/logout', function (req, res) {
+   delete req.session.auth;
+   res.send('<http><head><meta http-equiv="Refresh" content="1; /"><h1>Logged Out</h1></head>');
+});
+
+
+
+app.get('/get-articles', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT * FROM article ORDER BY date DESC', function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
-          res.send('User successfully created: ' + username);
+          res.send(JSON.stringify(result.rows));
       }
    });
 });
 
-
-app.post('/publish-article', function (req, res) {
-   // JSON
-   // var username = req.body.username;
-   // var password = req.body.password;
-
-    var articletitle = req.body.articletitle;
-    var articleheading = req.body.articleheading;
-    var articledate= req.body.articledate;
-    var articleauthor = req.body.articleauthor;
-    var articlecontent = req.body.articlecontent;
-
-    if (articletitle == '' || articleheading == '' || articleauthor == '' || articlecontent == '' ) {
-        // Inform the user on the screen through some message or give him a alert message
-        res.redirect('/publish-article');
-        return;
-    }
-    
-    //console.log(articledate);
-   pool.query('INSERT INTO article(title, heading, date, content, author) VALUES ($1, $2, $3, $4, $5)', [articletitle, articleheading, articledate, articlecontent, articleauthor], function (err, result) {
+app.get('/get-comments/:articleName', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT comment.*, "user".username FROM article, comment, "user" WHERE article.title = $1 AND article.id = comment.article_id AND comment.user_id = "user".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
       if (err) {
-          //res.status(500).send(err.toString());
-          res.redirect('/login');
+          res.status(500).send(err.toString());
       } else {
-          res.send(articletitle +' published successfully.');
+          res.send(JSON.stringify(result.rows));
       }
-    });
-
-   //setTimeout(res.redirect('/login'),5000);
+   });
 });
-
-
-
 
 app.post('/submit-comment/:articleName', function (req, res) {
    // Check if the user is logged in
@@ -335,7 +203,7 @@ app.post('/submit-comment/:articleName', function (req, res) {
                             if (err) {
                                 res.status(500).send(err.toString());
                             } else {
-                                res.status(200).send('Comment inserted!')
+                                res.status(200).send('Comment inserted!');
                             }
                         });
                 }
@@ -346,115 +214,29 @@ app.post('/submit-comment/:articleName', function (req, res) {
     }
 });
 
-app.get('/logout', function (req, res) {
-   delete req.session.auth;
-   res.redirect('/');
+app.get('/articles/:articleName', function (req, res) {
+  // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
+  pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
+    if (err) {
+        res.status(500).send(err.toString());
+    } else {
+        if (result.rows.length === 0) {
+            res.status(404).send('Article not found');
+        } else {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+    }
+  });
 });
 
 
-
-app.get('/publish-article', function (req, res) {
-     if (req.session && req.session.auth && req.session.auth.userId) {
-       res.sendFile(path.join(__dirname,'ui','publish-article.html'));
-
-     }
-     else{
-      res.send("Login to publish article");
-     }
-   
+app.get('/ui/:fileName', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
-
-
-app.get('/chatroom', function (req, res) {
-    res.sendFile(path.join(__dirname,'ui','chatroom.html'));
-});
-
-app.get('/readroom', function (req, res) {
-    res.sendFile(path.join(__dirname,'ui','readroom.html'));
-});
-
-
-app.get('/login', function (req, res) {
-    res.sendFile(path.join(__dirname,'ui','login.html'));
-});
-
-
-
-// serving static files 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','style.css'));
-});
-
-
-app.get('/ui/aboutstyle.css', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','aboutstyle.css'));
-});
-
-
-app.get('/ui/articlestyle.css', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','articlestyle.css'));
-});
-
-
-
-
-app.get('/ui/readroom.css', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','readroom.css'));
-});
-
-app.get('/ui/login.css', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','login.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','main.js'));
-});
-
-
-app.get('/ui/readroomscript.js', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','readroomscript.js'));
-});
-
-app.get('/ui/login.js', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','login.js'));
-});
-
-app.get('/ui/article.js', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','article.js'));
-});
-
-app.get('/ui/publish-article.js', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','publish-article.js'));
-});
-
-
-
-app.get('/ui/readroomjquery.min.js', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','readroomjquery.min.js'));
-});
-
-
-app.get('/ui/against.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','against.jpg'));
-});
-
-app.get('/ui/user.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','user.jpg'));
-});
-
-app.get('/ui/user1.JPG', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','user1.JPG'));
-});
-
-
-
-
-
-
-
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ` + port );
+  console.log(`IMAD course app listening on port ${port}!`);
 });
